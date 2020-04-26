c20b99f16a097419d1554e16a4c61290ba9c5739
Diego Almeida <diegalmeida@hotmail.com>
2020-04-26 00:52:23 -0300

    first commit

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    renderSingIn(req, res){
+        return res.render("consultation_page.njk");
+    },
+}
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const routes = express.Router();
+
+const UserController = require('../app/controllers/UserController');
+const HomeController = require('../app/controllers/HomeController');
+
+module.exports = routes;
