4981f8b418d07e89587d5d7c3c02b9a123bcc2ef
Axel <axeljo77@hotmail.com>
2020-04-28 16:55:47 -0300

    Codigo de requisição de dados da api + comentarios pra ser usado mais tarde

---
+++ src/api.js
@@ -0,0 +1,9 @@
+
+//Essa parte serve para requisitar/pegar os dados do api que vai ser recebido em formato json , ai a partir disso é possivel passar esses dados pro nunjuks e trabalhar com eles
+// A OUTRA PARTE DESSE ARQUIVO ESTA NA PASTA 'CONTROLLER' NO ARQUIVO 'UserController.Js"
+
+//cont axios = require('axios');
+
+//const api = axios.create({ baseURL : 'aqui vai a url da api'});
+
+//module.exports = api;
