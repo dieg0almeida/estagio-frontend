8185bb6e403c1032986c6357934cc70baa722029
AJP Valois <valois.antonioaugusto@gmail.com>
2020-04-30 09:18:17 -0300

    Criação das rotas e render próprias da página de consulta

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,17 +1,19 @@
-//antes de usar a variável vcs precisam importar ela.
+
 const api = require('../services/api');
 
 module.exports = {
 
-    //não precisa criar um método novo, pode fazer dentro do renderSignIn
-
     async renderSignIn(req, res) {
         try {
-            //a string que vc passa como parâmetro no método get
-            //são as rotas, o exemplo que eu dei lá no grupo estava assim
+            return res.render('login_page.njk');
+        } catch (err) {
+            console.warn('Erro na chamada da página de login.');
+        }
+    },
+
+    async renderConsultation(req, res) {
+        try {
             const response = await api.get('/owners');
-            //no response vem um monte de coisa, os dados ficam dentro do objeto data
-            //dentro do objeto data tem um array owners com os dados
             const owners = [...response.data.owners]
             console.log(owners);
             return res.render('consultation_page.njk', {owners});
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,5 +8,8 @@ const HomeController = require('../app/controllers/HomeController');
 routes.get('/', HomeController.index);
 routes.get('/sign_in', UserController.renderSignIn);
 
+routes.get('/consultation', UserController.renderConsultation);
+routes.post('/consultation', UserController.renderConsultation);
+
 
 module.exports = routes;
